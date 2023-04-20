4df980bf41e4b8e7a0a8b2260b663de8ae823270
ubique-complex <bhardwajparas518@gmail.com>
2023-04-20 20:04:21 +0530

    third commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, './client/build')));
 //   res.sendFile(path.join(__dirname, './client/build/index.html'));
 // });
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "https://ubique-creations.onrender.com/"
 }; // Use this after the variable declaration
 app.use(cors(corsOptions));
 app.use('/', indexRouter);
